d13d7773e0e55c430aa0de43c8422b26f2a0a1f2
sruthi <sruthi@mv.in>
2024-11-25 18:43:44 +0530

    url

--- src/vendorDashboard/data/ApiPath.js
+++ src/vendorDashboard/data/ApiPath.js
@@ -1,2 +1,2 @@
-//export const API_URL = "https://backend-Swiggy-project.onrender.com"
-export const API_URL = "http://localhost:4000"
+export const API_URL = "https://backend-Swiggy-project.onrender.com"
+//export const API_URL = "http://localhost:4000"
